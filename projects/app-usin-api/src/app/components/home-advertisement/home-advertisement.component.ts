632834e18aa860a61c9f907b5305f0aa6b62b7e0
Luis Gonzalez <luismaglz@live.com>
2020-03-04 19:07:55 -0700

    added inversify

---
+++ projects/app-usin-api/src/app/components/home-advertisement/home-advertisement.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-advertisement',
+  templateUrl: './home-advertisement.component.html',
+  styleUrls: ['./home-advertisement.component.css']
+})
+export class HomeAdvertisementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
