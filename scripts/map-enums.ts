3bf695895fdeaffe879b345c5b14d3cb137a48db
Luis <luismaglz@live.com>
2020-03-06 15:56:20 -0700

    added some scripts

---
+++ scripts/map-enums.ts
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+fs.readFile("../../swagger-gen/types/enums.ts", "utf8", function(
+  err,
+  contents
+) {
+  // console.log(contents);
+  const regex = /^export enum .* {\r([\s\S]*?)}/gm;
+  const matches = contents.match(regex);
+
+  console.log(contents.match(regex)[0]);
+});
