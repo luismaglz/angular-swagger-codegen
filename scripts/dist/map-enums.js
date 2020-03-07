92b027b1ef69aa9d8397edadfbe154cc3c88d88e
Luis Gonzalez <luismaglz@live.com>
2020-03-07 09:02:13 -0700

    updates to script

--- scripts/dist/map-enums.js
+++ scripts/dist/map-enums.js
@@ -1,8 +1,34 @@
 const fs = require("fs");
-fs.readFile("../../swagger-gen/types/enums.ts", "utf8", function (err, contents) {
+const replace = require("replace-in-file");
+const enumsPath = "../string-enums.txt";
+const modelsPath = "../../projects/ts-dotrez-api/model/*.ts";
+const dry = true;
+// Read and process enums
+fs.readFile(enumsPath, "utf8", processEnums);
+function processEnums(err, contents) {
     // console.log(contents);
-    const regex = /^export enum .* {\r([\s\S]*?)}/gm;
-    const matches = contents.match(regex);
-    console.log(contents.match(regex)[0]);
-});
+    const enumDictionary = JSON.parse(contents);
+    // console.log(Object.keys(enumDictionary)[0].replace(/"/g, "'"));
+    // const regex = /^export enum .* {\r([\s\S]*?)}/gm;
+    // const matches = contents.match(regex);
+    const options = {
+        files: modelsPath,
+        from: /<ENUM>(.*)<\/ENUM>/gm,
+        to: (m, g) => replaceEnums(m, g, enumDictionary),
+        dry
+    };
+    replace(options)
+        .then(results => {
+        console.log("Replacement results:", results);
+    })
+        .catch(error => {
+        console.error("Error occurred:", error);
+    });
+}
+function replaceEnums(match, group, enumDictionary) {
+    if (typeof group !== "string")
+        return "";
+    const enumKey = group.replace(/"/g, "'");
+    enumDictionary[enumKey] ? enumDictionary[enumKey] : match;
+}
 //# sourceMappingURL=map-enums.js.map
--- scripts/map-enums.ts
+++ scripts/map-enums.ts
@@ -1,12 +1,38 @@
 const fs = require("fs");
+const replace = require("replace-in-file");
 
-fs.readFile("../../swagger-gen/types/enums.ts", "utf8", function(
-  err,
-  contents
-) {
+const enumsPath = "../string-enums.txt";
+const modelsPath = "../../projects/ts-dotrez-api/model/*.ts";
+const dry = true;
+
+// Read and process enums
+fs.readFile(enumsPath, "utf8", processEnums);
+
+function processEnums(err, contents) {
   // console.log(contents);
-  const regex = /^export enum .* {\r([\s\S]*?)}/gm;
-  const matches = contents.match(regex);
+  const enumDictionary = JSON.parse(contents);
+  // console.log(Object.keys(enumDictionary)[0].replace(/"/g, "'"));
+  // const regex = /^export enum .* {\r([\s\S]*?)}/gm;
+  // const matches = contents.match(regex);
 
-  console.log(contents.match(regex)[0]);
-});
+  const options = {
+    files: modelsPath,
+    from: /<ENUM>(.*)<\/ENUM>/gm,
+    to: (m, g) => replaceEnums(m, g, enumDictionary),
+    dry
+  };
+
+  replace(options)
+    .then(results => {
+      console.log("Replacement results:", results);
+    })
+    .catch(error => {
+      console.error("Error occurred:", error);
+    });
+}
+
+function replaceEnums(match: string, group: string, enumDictionary: string) {
+  if (typeof group !== "string") return "";
+  const enumKey = group.replace(/"/g, "'");
+  enumDictionary[enumKey] ? enumDictionary[enumKey] : match;
+}
