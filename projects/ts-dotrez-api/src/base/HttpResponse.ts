843f0f267bac65b7d145e0636914fc195dea3c9d
Luis Gonzalez <luismaglz@live.com>
2020-03-08 12:18:07 -0600

    another gen

--- projects/ts-dotrez-api/src/base/HttpResponse.ts
+++ projects/ts-dotrez-api/src/base/HttpResponse.ts
@@ -1,11 +1,9 @@
 import { Headers } from './Headers';
 
-class HttpResponse<T = any> {
+export class HttpResponse<T = any> {
   constructor(
     public response: T,
     public status: number,
     public headers?: Headers
   ) {}
 }
-
-export default HttpResponse;
